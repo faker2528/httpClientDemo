8c06369f79a308a3e3e89210de6dae86d4bfa1a3
刘鑫 <liuxin1@szkingdom.com>
2025-06-20 15:24:01 +0800

    Revert "提交HttpClientDemo项目，目前就完成了三个部分：每日资讯，文字转语音，天气预报"
    This reverts commit 938d9dbf979df92699d808f9f25cb337e61a91a1.

--- src/main/java/org/example/service/AudioService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import org.example.generic.GenericResult;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AudioService {
-    GenericResult transferText(String text, String speed) throws Exception;
-}
--- src/main/java/org/example/service/NewsService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import org.example.generic.GenericResult;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface NewsService {
-    public GenericResult getNews();
-}
